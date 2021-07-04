eba7127e55acda84b2946af9e112b32ab3aa34de
Nemolite <g25092011@mail.ru>
2021-07-04 08:47:02 +0300

    update

--- page-news.php
+++ page-news.php
@@ -2,4 +2,48 @@
 /*
 Template Name: для списка Новостей
 */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+} // Exit if accessed directly
+
+get_header(); 
 ?>
+<?php if ( have_posts() ) : the_post(); ?>
+
+<?php posterity_title_bar(); ?>
+
+<article id="content" class="clearfix">
+    <div class="content-limiter">
+        <div id="col-mask">
+
+            <div id="post-<?php the_ID(); ?>" <?php post_class('content-box'); ?>>
+                <div class="formatter">
+                    <?php posterity_title_bar( 'inside' ); ?>
+                    <div class="real-content">
+
+                        <div class="chv-news-list">
+
+<div class="chv-news-one">
+</div>
+
+<div class="chv-news-one">
+</div>
+
+<div class="chv-news-one">
+</div>
+
+                        </div>
+                      
+
+                    </div>
+                </div>
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</article>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
